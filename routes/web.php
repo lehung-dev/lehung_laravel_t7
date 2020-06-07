e21295f6089b669a529ba29c3d1877f89d41b645
lehung <lehung.3011@gmail.com>
2020-06-08 01:00:10 +0700

    W1_T1_laravel_split_routes_file

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,72 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-$prefixAdmin = config('zvn.url.prefix_admin');
+
 $prefixNews  = config('zvn.url.prefix_news');
-
-Route::group(['prefix' => $prefixAdmin, 'namespace' => 'Admin', 'middleware' => ['permission.admin']], function () {
-    // ============================== DASHBOARD ==============================
-    $prefix         = 'dashboard';
-    $controllerName = 'dashboard';
-    Route::group(['prefix' =>  $prefix], function () use ($controllerName) {
-        $controller = ucfirst($controllerName)  . 'Controller@';
-        Route::get('/',                             [ 'as' => $controllerName,                  'uses' => $controller . 'index' ]);
-    });
-
-    // ============================== SLIDER ==============================
-    $prefix         = 'slider';
-    $controllerName = 'slider';
-    Route::group(['prefix' =>  $prefix], function () use ($controllerName) {
-        $controller = ucfirst($controllerName)  . 'Controller@';
-        Route::get('/',                             [ 'as' => $controllerName,                  'uses' => $controller . 'index' ]);
-        Route::get('form/{id?}',                    [ 'as' => $controllerName . '/form',        'uses' => $controller . 'form'])->where('id', '[0-9]+');
-        Route::post('save',                         [ 'as' => $controllerName . '/save',        'uses' => $controller . 'save']);
-        Route::get('delete/{id}',                   [ 'as' => $controllerName . '/delete',      'uses' => $controller . 'delete'])->where('id', '[0-9]+');
-        Route::get('change-status-{status}/{id}',   [ 'as' => $controllerName . '/status',      'uses' => $controller . 'status'])->where('id', '[0-9]+');
-    });
-
-    // ============================== CATEGORY ==============================
-    $prefix         = 'category';
-    $controllerName = 'category';
-    Route::group(['prefix' =>  $prefix], function () use ($controllerName) {
-        $controller = ucfirst($controllerName)  . 'Controller@';
-        Route::get('/',                                 [ 'as' => $controllerName,                  'uses' => $controller . 'index' ]);
-        Route::get('form/{id?}',                        [ 'as' => $controllerName . '/form',        'uses' => $controller . 'form'])->where('id', '[0-9]+');
-        Route::post('save',                             [ 'as' => $controllerName . '/save',        'uses' => $controller . 'save']);
-        Route::get('delete/{id}',                       [ 'as' => $controllerName . '/delete',      'uses' => $controller . 'delete'])->where('id', '[0-9]+');
-        Route::get('change-status-{status}/{id}',       [ 'as' => $controllerName . '/status',      'uses' => $controller . 'status'])->where('id', '[0-9]+');
-        Route::get('change-is-home-{isHome}/{id}',      [ 'as' => $controllerName . '/isHome',      'uses' => $controller . 'isHome'])->where('id', '[0-9]+');
-        Route::get('change-display-{display}/{id}',     [ 'as' => $controllerName . '/display',     'uses' => $controller . 'display']);
-    });
-
-    // ============================== ARTICLE ==============================
-    $prefix         = 'article';
-    $controllerName = 'article';
-    Route::group(['prefix' =>  $prefix], function () use($controllerName) {
-        $controller = ucfirst($controllerName)  . 'Controller@';
-        Route::get('/',                                 [ 'as' => $controllerName,                  'uses' => $controller . 'index' ]);
-        Route::get('form/{id?}',                        [ 'as' => $controllerName . '/form',        'uses' => $controller . 'form'])->where('id', '[0-9]+');
-        Route::post('save',                             [ 'as' => $controllerName . '/save',        'uses' => $controller . 'save']);
-        Route::get('delete/{id}',                       [ 'as' => $controllerName . '/delete',      'uses' => $controller . 'delete'])->where('id', '[0-9]+');
-        Route::get('change-status-{status}/{id}',       [ 'as' => $controllerName . '/status',      'uses' => $controller . 'status']);
-        Route::get('change-type-{type}/{id}',           [ 'as' => $controllerName . '/type',        'uses' => $controller . 'type']);
-    });
-
-    // ============================== USER ==============================
-    $prefix         = 'user';
-    $controllerName = 'user';
-    Route::group(['prefix' =>  $prefix], function () use($controllerName) {
-        $controller = ucfirst($controllerName)  . 'Controller@';
-        Route::get('/',                                 [ 'as' => $controllerName,                  'uses' => $controller . 'index' ]);
-        Route::get('form/{id?}',                        [ 'as' => $controllerName . '/form',        'uses' => $controller . 'form'])->where('id', '[0-9]+');
-        Route::post('save',                             [ 'as' => $controllerName . '/save',        'uses' => $controller . 'save']);
-        Route::post('change-password',                  [ 'as' => $controllerName . '/change-password',        'uses' => $controller . 'changePassword']);
-        Route::post('change-level',                     [ 'as' => $controllerName . '/change-level',        'uses' => $controller . 'changeLevel']);
-        Route::get('delete/{id}',                       [ 'as' => $controllerName . '/delete',      'uses' => $controller . 'delete'])->where('id', '[0-9]+');
-        Route::get('change-status-{status}/{id}',       [ 'as' => $controllerName . '/status',      'uses' => $controller . 'status']);
-        Route::get('change-level-{level}/{id}',         [ 'as' => $controllerName . '/level',      'uses' => $controller . 'level']);
-    });
-});
 
 Route::group(['prefix' => $prefixNews, 'namespace' => 'News'], function () {
     // ============================== HOMEPAGE ==============================
